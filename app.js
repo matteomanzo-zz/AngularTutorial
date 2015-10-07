b8d4784f48d690b8ea6699c7be49aa9a62d695ba
matteomanzo <matteo.manzo@comparethemarket.com>
2015-10-07 11:37:13 +0100

    wrapped gems into an array and iterate inside on the html

--- app.js
+++ app.js
@@ -2,15 +2,25 @@
   var app = angular.module('store', [ ]);
 
   app.controller('StoreController', function(){
-    this.product = gem;
+    this.product = gems;
   });
 
-  var gems = {
+  var gems = [
+    {
       name: 'Dodecahedron',
       price: 2.95,
       description: 'This gem is a dodecahedron',
       canPurchase: true,
       soldOut: true,
-  }
+    },
+
+    {
+      name: 'Pentagonal Gem',
+      price: 5.95,
+      description: 'This is a Pentagonal Gem',
+      canPurchase: true,
+      soldOut: false,
+    }
+  ]
 
 })();
